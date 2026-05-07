c54402788154fdcac0258ed786701f1e612075d8
nammon14945 <narmmon_o@softsquaregroup.com>
2026-05-08 00:48:08 +0700

    return data to client by ResponseEntity

--- src/main/java/com/itemmanagement/backend/controller/TestController.java
+++ src/main/java/com/itemmanagement/backend/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.itemmanagement.backend.controller;
 import com.itemmanagement.backend.model.Product;
 import com.itemmanagement.backend.model.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -87,7 +88,7 @@ public class TestController {
     // แล้วเอา object นั้นส่งไปยังหน้าบ้าน
     @GetMapping("/api/product/{id}")
 
-    // ชื่อตัวแปรต้องตรงกับ /{id}
+    // ชื่อตัวแปรต้องตรงกับ /{id} หรือจะใช้ชื่อตัวแปรไม่ตรง (@PathVariable("id") Long ids) โดยให้ PathVariable เป็นตัวช่วย binding ให้ได้
     public Product getProductById(@PathVariable Long id) {
 
         // แทนค่าตัวแปร products คือรายการ objects ที่กำลังจะเพิ่มเข้าไปใน class Products
@@ -100,9 +101,50 @@ public class TestController {
         );
 
         // ส่ง object ใน Product ที่มี Path id ตรงกับ Product id ไปยังหน้าบ้าน
+        // .stream() คือการเอา List products มาเปิดเป็น Stream เพื่อเตรียมประมวลผลข้อมูลทีละตัว
+        // Stream เหมือนการนำข้อมูลทั้งหมดใน products มาวางบนสาพาน แล้วจับขึ้นมาดูทีละตัวเพื่อหาว่าข้อมูลของรายการนั้นตรงกันกับเงื่อนไขไหม
+        return products.stream()
+
+                // กรองเอาเฉพาะ product ที่ product.getId() ตรงกับ ่รับมาจาก path id
+                .filter(product -> product.getId().equals(id))
+
+                // เอาตัวแรกที่กรองเจอ
+                .findFirst()
+
+                // ถ้าไม่เจอสินค้าเลย ให้คืนค่าเป็น null
+                .orElse(null);
+    }
+
+    // สร้าง object ใน Product(Object Class) ขึ้นมาใหม่ แล้วไปอ่าน Product ว่ามีรายการไหนที่มี id ตรงกับ /{id} บ้าง
+    // แล้วเอา object นั้นส่งไปหน้าบ้าน
+    @GetMapping("/api/products/{id}")
+
+    // หมายถึง method นี้จะ return ข้อมูลไปเป็น HTTP Respone ที่มี body เป็น Product
+    // ResponseEntity ดีตรงที่เราควบคุม HTTP status ได้
+    public ResponseEntity<Product> getProductByIdSecond(@PathVariable Long id) {
+        List<Product> products = List.of(
+                new Product(101L, "Keyboard", 1500, true),
+                new Product(102L, "Mouse", 700, true),
+                new Product(103L, "Monitor", 4500, false)
+        );
+
+        // .steam() จับข้อมูลวางเรียง แล้วหยิบบขึ้นทีละรายการเพื่อเช็คเงื่อนไข หากถูกก็เลือกมา
         return products.stream()
                 .filter(product -> product.getId().equals(id))
                 .findFirst()
-                .orElse(null);
+
+                // ถ้าเจอข้อมูลมันจะส่ง object ที่ตรงเงื่อนไขด้านบน แล้วห่อด้วย ReponseEntity อีกรอบ ที่กล่าวคือ
+                // Status: 200 OK
+                //    Body:
+                //    {
+                //      "id": 101,
+                //      "name": "Keyboard",
+                //      "price": 1500,
+                //      "inStock": true
+                //    }
+                // แปลตามตัว product(เป็นถ้ามีข้อมูล) ->(ให้ทำการ) ResponseEntity.ok(product(นำ product มาห่อด้วย ResponseEntity)
+                .map(test -> ResponseEntity.ok(test))
+                // กรณีไม่จอ ให้ส่ง ResponseEntity แบบ notFound
+                .orElse(ResponseEntity.notFound().build());
     }
 }
